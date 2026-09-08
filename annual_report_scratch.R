f8cdc9499d6816cb8ec1a36b370cc09a6f8d916c
Monica Gucciardi <monica.gucciardi@phila.gov>
2026-09-08 16:22:34 -0400

    Download UI stubs

--- annual_report_scratch.R
+++ annual_report_scratch.R
@@ -145,22 +145,50 @@ table_6_7 <- reactive({
 	})
 	
 ########## UI output
-output$`Summary of Post-Construction CWL Monitoring of Public SMPs` <- renderReactable(reactable(table_5_1), striped = TRUE, pagination = FALSE)
-output$`Post-Construction CWL Monitoring of Public SMPs Listed by Type` <- renderReactable(reactable(table_5_2), striped = TRUE, pagination = FALSE)
-output$`Post-Construction SRTs performed on Public Systems` <- renderReactable(reactable(table_5_3), striped = TRUE, pagination = FALSE)
-output$`Public Systems with Post-Construction SRTs Performed` <- renderReactable(reactable(table_5_4), striped = TRUE, pagination = FALSE)
-output$`Construction-Phase SRTs Performed on Public Systems` <- renderReactable(reactable(table_5_5), striped = TRUE, pagination = FALSE)
-output$`Public Systems with Construction-Phase SRTs Performed` <- renderReactable(reactable(table_5_6), striped = TRUE, pagination = FALSE)
-output$`Public Systems with CETs Administered` <- renderReactable(reactable(table_5_7), striped = TRUE, pagination = FALSE)
-output$`Public Systems with Infiltration Testing Administered` <- renderReactable(reactable(table_5_8), striped = TRUE, pagination = FALSE)
-output$`Public Systems with Inlet Leakage Tests Administered` <- renderReactable(reactable(table_5_9), striped = TRUE, pagination = FALSE)
-output$`Inlet Conveyance Tests Performed on Public Systems` <- renderReactable(reactable(table_5_10), striped = TRUE, pagination = FALSE)
-output$`Groundwater Monitoring for Public GSI` <- renderReactable(reactable(table_5_11), striped = TRUE, pagination = FALSE)
-output$`Summary of Post-Construction CWL Monitoring of Private Systems` <- renderReactable(reactable(table_6_1), striped = TRUE, pagination = FALSE)
-output$`Post-Construction CWL Monitoring of Private Systems Listed by Type` <- renderReactable(reactable(table_6_2), striped = TRUE, pagination = FALSE)
-output$`Post-Construction SRTs performed on Private Systems` <- renderReactable(reactable(table_6_3), striped = TRUE, pagination = FALSE)
-output$`Private SMPs with Post-Construction SRTs Performed` <- renderReactable(reactable(table_6_4), striped = TRUE, pagination = FALSE)
-output$`Private Systems with CETs Administered` <- renderReactable(reactable(table_6_5), striped = TRUE, pagination = FALSE)
-output$`Private Systems with ICTs Administered` <- renderReactable(reactable(table_6_6), striped = TRUE, pagination = FALSE)
-output$`Private Systems with WWIs Administered` <- renderReactable(reactable(table_6_7), striped = TRUE, pagination = FALSE)
+output$`Summary of Post-Construction CWL Monitoring of Public SMPs` <- renderReactable(reactable(table_5_1()), striped = TRUE, pagination = FALSE)
+output$`Post-Construction CWL Monitoring of Public SMPs Listed by Type` <- renderReactable(reactable(table_5_2()), striped = TRUE, pagination = FALSE)
+output$`Post-Construction SRTs performed on Public Systems` <- renderReactable(reactable(table_5_3()), striped = TRUE, pagination = FALSE)
+output$`Public Systems with Post-Construction SRTs Performed` <- renderReactable(reactable(table_5_4()), striped = TRUE, pagination = FALSE)
+output$`Construction-Phase SRTs Performed on Public Systems` <- renderReactable(reactable(table_5_5()), striped = TRUE, pagination = FALSE)
+output$`Public Systems with Construction-Phase SRTs Performed` <- renderReactable(reactable(table_5_6()), striped = TRUE, pagination = FALSE)
+output$`Public Systems with CETs Administered` <- renderReactable(reactable(table_5_7()), striped = TRUE, pagination = FALSE)
+output$`Public Systems with Infiltration Testing Administered` <- renderReactable(reactable(table_5_8()), striped = TRUE, pagination = FALSE)
+output$`Public Systems with Inlet Leakage Tests Administered` <- renderReactable(reactable(table_5_9()), striped = TRUE, pagination = FALSE)
+output$`Inlet Conveyance Tests Performed on Public Systems` <- renderReactable(reactable(table_5_10()), striped = TRUE, pagination = FALSE)
+output$`Groundwater Monitoring for Public GSI` <- renderReactable(reactable(table_5_11()), striped = TRUE, pagination = FALSE)
+output$`Summary of Post-Construction CWL Monitoring of Private Systems` <- renderReactable(reactable(table_6_1()), striped = TRUE, pagination = FALSE)
+output$`Post-Construction CWL Monitoring of Private Systems Listed by Type` <- renderReactable(reactable(table_6_2()), striped = TRUE, pagination = FALSE)
+output$`Post-Construction SRTs performed on Private Systems` <- renderReactable(reactable(table_6_3()), striped = TRUE, pagination = FALSE)
+output$`Private SMPs with Post-Construction SRTs Performed` <- renderReactable(reactable(table_6_4()), striped = TRUE, pagination = FALSE)
+output$`Private Systems with CETs Administered` <- renderReactable(reactable(table_6_5()), striped = TRUE, pagination = FALSE)
+output$`Private Systems with ICTs Administered` <- renderReactable(reactable(table_6_6()), striped = TRUE, pagination = FALSE)
+output$`Private Systems with WWIs Administered` <- renderReactable(reactable(table_6_7()), striped = TRUE, pagination = FALSE)
 
+          output$download_table <- downloadHandler(
+            
+            filename = function() {
+              paste("FY",input$fy,"_","AnnualReport","_",Sys.Date(),".xlsx", sep = "")
+            },
+            content = function(filename){
+              
+              df_list <- list(table_5_1 = table_5_1(),
+								table_5_2 = table_5_2(),
+								table_5_3 = table_5_3(),
+								table_5_4 = table_5_4(),
+								table_5_5 = table_5_5(),
+								table_5_6 = table_5_6(),
+								table_5_7 = table_5_7(),
+								table_5_8 = table_5_8(),
+								table_5_9 = table_5_9(),
+								table_5_10 = table_5_10(),
+								table_5_11 = table_5_11(),
+								table_6_1 = table_6_1(),
+								table_6_2 = table_6_2(),
+								table_6_3 = table_6_3(),
+								table_6_4 = table_6_4(),
+								table_6_5 = table_6_5(),
+								table_6_6 = table_6_6(),
+								table_6_7 = table_6_7())
+              write.xlsx(x = df_list , file = filename, rowNames = TRUE)
+            }
+          ) 
