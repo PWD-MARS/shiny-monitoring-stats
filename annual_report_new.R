a1b8ae78c7b1dc92e4f42ad1df3f8d8724c4c7cd
Monica Gucciardi <monica.gucciardi@phila.gov>
2026-09-16 10:33:53 -0400

    Table 6

--- annual_report_new.R
+++ annual_report_new.R
@@ -36,10 +36,10 @@ a_reportUI <- function(id, label = "a_report", current_fy, years){
                 reactableOutput(ns("Public Systems with Post-Construction SRTs Performed")),
                 
               strong("Table 5-5: Construction-Phase SRTs Performed on Public Systems"),
-                reactableOutput(ns("Construction-Phase SRTs Performed on Public Systems")) #,
+                reactableOutput(ns("Construction-Phase SRTs Performed on Public Systems")),
                 
-              # strong("Table 5-6: Public Systems with Construction-Phase SRTs Performed"),
-              #   reactableOutput(ns("Public Systems with Construction-Phase SRTs Performed")),
+              strong("Table 5-6: Public Systems with Construction-Phase SRTs Performed"),
+                reactableOutput(ns("Public Systems with Construction-Phase SRTs Performed")) #,
                 
               # strong("Table 5-7: Public Systems with CETs Administered"),
               #   reactableOutput(ns("Public Systems with CETs Administered")),
@@ -379,6 +379,56 @@ a_reportServer <- function(id, parent_session, current_fy, poolConn){
           return(public_midcon_srt)
         })
 
+        table_5_6 <- reactive({
+
+          #Public systems recieving mid-con SRTs this FY
+          fy_public_midcon_srt_systems <-"select sfc.asset_type, count(distinct(srt.system_id))
+                                        from fieldwork.viw_srt_full srt
+                                        left join external.mat_assets sfc on srt.system_id = sfc.system_id
+                                        where sfc.component_id is null
+                                        and test_date >= '%s'
+                                        and test_date <= '%s'
+                                        and phase = 'Construction'
+                                        and public = TRUE
+                                        group by sfc.asset_type"
+
+          fy_public_midcon_srt_systems_prod <- dbGetQuery(poolConn, 
+                                                 paste(sprintf(fy_public_midcon_srt_systems, 
+                                                               FYSTART_reactive(), 
+                                                               FYEND_reactive()),
+                                                       collapse=""))
+
+          #Public systems recieving mid-con SRTs to date
+          todate_public_midcon_srt_systems <-"select sfc.asset_type, count(distinct(srt.system_id))
+                                        from fieldwork.viw_srt_full srt
+                                        left join external.mat_assets sfc on srt.system_id = sfc.system_id
+                                        where sfc.component_id is null
+                                        and test_date <= '%s'
+                                        and phase = 'Construction'
+                                        and public = TRUE
+                                        group by sfc.asset_type"
+          todate_public_midcon_srt_systems_prod <- dbGetQuery(poolConn, 
+                                                          paste(sprintf(todate_public_midcon_srt_systems,
+                                                                        FYEND_reactive()),
+                                                                collapse=""))
+
+          #Assembling output table
+          public_midcon_srt_bysystem <- left_join(todate_public_midcon_srt_systems_prod,
+                                          fy_public_midcon_srt_systems_prod,
+                                          by = "asset_type",
+                                          suffix = c(".todate", ".fy"))
+
+          rownames(public_midcon_srt_bysystem)<- public_midcon_srt_bysystem$asset_type
+          public_midcon_srt_bysystem <- transmute(public_midcon_srt_bysystem,
+                                       "This Fiscal Year" = replace_na(count.fy, 0),
+                                       "To Date" = count.todate)
+
+          return(public_midcon_srt_bysystem)
+
+
+        })
+
+
         
         #reactable table outputs
         output$`Summary of Post-Construction CWL Monitoring of Public SMPs` <- renderReactable(reactable(table_5_1(), striped = TRUE, pagination = FALSE))
@@ -386,7 +436,7 @@ a_reportServer <- function(id, parent_session, current_fy, poolConn){
         output$`Post-Construction SRTs performed on Public Systems` <- renderReactable(reactable(table_5_3(), striped = TRUE, pagination = FALSE))
         output$`Public Systems with Post-Construction SRTs Performed` <- renderReactable(reactable(table_5_4(), striped = TRUE, pagination = FALSE))
         output$`Construction-Phase SRTs Performed on Public Systems` <- renderReactable(reactable(table_5_5(), striped = TRUE, pagination = FALSE))
-        # output$`Public Systems with Construction-Phase SRTs Performed` <- renderReactable(reactable(table_5_6(), striped = TRUE, pagination = FALSE))
+        output$`Public Systems with Construction-Phase SRTs Performed` <- renderReactable(reactable(table_5_6(), striped = TRUE, pagination = FALSE))
         # output$`Public Systems with CETs Administered` <- renderReactable(reactable(table_5_7(), striped = TRUE, pagination = FALSE))
         # output$`Public Systems with Infiltration Testing Administered` <- renderReactable(reactable(table_5_8(), striped = TRUE, pagination = FALSE))
         # output$`Public Systems with Inlet Leakage Tests Administered` <- renderReactable(reactable(table_5_9(), striped = TRUE, pagination = FALSE))
