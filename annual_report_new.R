f73701ae9a25e130531fd1a19f7ee7485b5ed729
Monica Gucciardi <monica.gucciardi@phila.gov>
2026-09-16 16:01:53 -0400

    Removed table numbers from display names

--- annual_report_new.R
+++ annual_report_new.R
@@ -23,83 +23,83 @@ a_reportUI <- function(id, label = "a_report", current_fy, years){
              mainPanel(
                
                
-              strong("Table 5-1: Summary of Post-Construction CWL Monitoring of Public SMPs"),
+              strong("Summary of Post-Construction CWL Monitoring of Public SMPs"),
                 reactableOutput(ns("Summary of Post-Construction CWL Monitoring of Public SMPs")),
                 br(),
                 
-              strong("Table 5-2: Post-Construction CWL Monitoring of Public SMPs Listed by Type"),
+              strong("Post-Construction CWL Monitoring of Public SMPs Listed by Type"),
                 reactableOutput(ns("Post-Construction CWL Monitoring of Public SMPs Listed by Type")),
                 br(),
                 
-              strong("Table 5-3: Post-Construction SRTs performed on Public Systems"),
+              strong("Post-Construction SRTs performed on Public Systems"),
                 reactableOutput(ns("Post-Construction SRTs performed on Public Systems")),
                 br(),
                 
-              strong("Table 5-4: Public Systems with Post-Construction SRTs Performed"),
+              strong("Public Systems with Post-Construction SRTs Performed"),
                 reactableOutput(ns("Public Systems with Post-Construction SRTs Performed")),
                 br(),
                 
-              strong("Table 5-5: Construction-Phase SRTs Performed on Public Systems"),
+              strong("Construction-Phase SRTs Performed on Public Systems"),
                 reactableOutput(ns("Construction-Phase SRTs Performed on Public Systems")),
                 br(),
                 
-              strong("Table 5-6: Public Systems with Construction-Phase SRTs Performed"),
+              strong("Public Systems with Construction-Phase SRTs Performed"),
                 reactableOutput(ns("Public Systems with Construction-Phase SRTs Performed")),
                 br(),
                 
-              strong("Table 5-7: Public Systems with CETs Administered"),
+              strong("Public Systems with CETs Administered"),
                 reactableOutput(ns("Public Systems with CETs Administered")),
                 br(),
                 
-              strong("Table 5-8: Public Systems with Infiltration Testing Administered"),
+              strong("Public Systems with Infiltration Testing Administered"),
                 reactableOutput(ns("Public Systems with Infiltration Testing Administered")),
                 br(),
                 
-              strong("Table 5-9: Public Systems with Inlet Leakage Tests Administered"),
+              strong("Public Systems with Inlet Leakage Tests Administered"),
                 reactableOutput(ns("Public Systems with Inlet Leakage Tests Administered")),
               br(),
                 
-              strong("Table 5-10: Public Systems with ICTs Administered"),
+              strong("Public Systems with ICTs Administered"),
                 reactableOutput(ns("Public Systems with ICTs Administered")),
               br(),
                 
-              strong("Table 5-11: Public Systems with Groundwater Monitoring"),
+              strong("Public Systems with Groundwater Monitoring"),
                 reactableOutput(ns("Public Systems with Groundwater Monitoring")),
               br(),
                 
-              strong("Table 6-1: Summary of Post-Construction CWL Monitoring of Private Systems"),
+              strong("Summary of Post-Construction CWL Monitoring of Private Systems"),
                 reactableOutput(ns("Summary of Post-Construction CWL Monitoring of Private Systems")),
               br(),
                 
-              strong("Table 6-2: Post-Construction CWL Monitoring of Private Systems Listed by Type"),
+              strong("Post-Construction CWL Monitoring of Private Systems Listed by Type"),
                 reactableOutput(ns("Post-Construction CWL Monitoring of Private Systems Listed by Type")),
               br(),
                 
-              strong("Table 6-3: Post-Construction SRTs performed on Private Systems"),
+              strong("Post-Construction SRTs performed on Private Systems"),
                 reactableOutput(ns("Post-Construction SRTs performed on Private Systems")),
               br(),
                 
-              strong("Table 6-4: Private SMPs with Post-Construction SRTs Performed"),
+              strong("Private SMPs with Post-Construction SRTs Performed"),
                 reactableOutput(ns("Private SMPs with Post-Construction SRTs Performed")),
               br(),
                 
-              strong("Table 6-5: Private Systems with CETs Administered"),
+              strong("Private Systems with CETs Administered"),
                 reactableOutput(ns("Private Systems with CETs Administered")),
               br(),
                 
-              strong("Table 6-6: Private Systems with Inlet Leakage Tests Administered"),
+              strong("Private Systems with Inlet Leakage Tests Administered"),
                 reactableOutput(ns("Private Systems with Inlet Leakage Tests Administered")),
               br(),
 
-              strong("Table 6-7: Private Systems with ICTs Administered"),
+              strong("Private Systems with ICTs Administered"),
                 reactableOutput(ns("Private Systems with ICTs Administered")),
               br(),
                 
-              strong("Table 6-8: Private Systems with WWIs Administered"),
+              strong("Private Systems with WWIs Administered"),
                 reactableOutput(ns("Private Systems with WWIs Administered")),
                 br(),
 
-              strong("Table 7-1: Collection System Dye Tests Administered"),
+              strong("Collection System Dye Tests Administered"),
                 reactableOutput(ns("Collection System Dye Tests Administered"))
              )
            )
